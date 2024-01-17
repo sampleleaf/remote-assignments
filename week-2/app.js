2c8df4e73f59a62b2baf62335a8fa43403e727ff
Benny Chang <c.a.s.winterleaf@gmail.com>
2024-01-17 15:31:11 +0800

    week2-Request1

---
+++ week-2/app.js
@@ -0,0 +1,7 @@
+/* ====================
+   Click to Change Text
+==================== */
+let title = document.querySelector('#title')
+title.addEventListener('click', () => {
+    title.textContent = 'Have a Good Time!'
+})
