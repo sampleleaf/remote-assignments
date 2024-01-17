cb36ef1e2bf0c9f5b7e4f2aa795ea95fde154f02
Benny Chang <c.a.s.winterleaf@gmail.com>
2024-01-17 18:42:31 +0800

    week2-Request2

--- week-2/app.js
+++ week-2/app.js
@@ -5,3 +5,20 @@ let title = document.querySelector('#title')
 title.addEventListener('click', () => {
     title.textContent = 'Have a Good Time!'
 })
+
+/* ====================
+   Click to Show/Close Menu
+==================== */
+let navbar = document.querySelector('#navbar')
+let navTrigger = document.querySelector('#nav-trigger')
+let navCross = document.querySelector('#nav-cross')
+
+navTrigger.addEventListener('click', function(){
+    navbar.classList.add('rwd-nav')
+    navbar.classList.remove('hide-nav') //show menu
+})
+
+navCross.addEventListener('click', function(){
+    navbar.classList.remove('rwd-nav')
+    navbar.classList.add('hide-nav') //close menu
+})
