ddcdd115dcde813c969aeeefeac6c13eddaf5bed
Benny Chang <c.a.s.winterleaf@gmail.com>
2024-01-17 19:13:25 +0800

    week2-Request3

--- week-2/app.js
+++ week-2/app.js
@@ -13,12 +13,22 @@ let navbar = document.querySelector('#navbar')
 let navTrigger = document.querySelector('#nav-trigger')
 let navCross = document.querySelector('#nav-cross')
 
-navTrigger.addEventListener('click', function(){
+navTrigger.addEventListener('click', () => {
     navbar.classList.add('rwd-nav')
     navbar.classList.remove('hide-nav') //show menu
 })
 
-navCross.addEventListener('click', function(){
+navCross.addEventListener('click', () => {
     navbar.classList.remove('rwd-nav')
     navbar.classList.add('hide-nav') //close menu
+})
+
+/* ====================
+   Click to Show More Content Boxes
+==================== */
+let showContent = document.querySelector('#showContent')
+let hiddenSec = document.querySelector('#hiddenSec')
+
+showContent.addEventListener('click', () => {
+    hiddenSec.classList.remove('hide')
 })
