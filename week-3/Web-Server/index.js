0582e484b1926aa6cef829665980d2699f797942
Benny Chang <c.a.s.winterleaf@gmail.com>
2024-01-25 12:45:52 +0800

    week3-assignment2

--- week-3/Web-Server/index.js
+++ week-3/Web-Server/index.js
@@ -4,6 +4,23 @@ const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello, My Server!')
+}) 
+
+app.get('/getData', (req, res) => {
+    const { number } = req.query;
+    if(!number){
+        res.send('Lack of Parameter')
+    }else if(number === 'POSITIVE_INTEGER'){
+        res.send(`1+2+....+${number}`)
+    }else if(isNaN(number) || number == 0){//字串0或數字0都不要
+        res.send('Wrong Parameter')
+    }else{
+        let sum = 0;
+        for(let i = 1; i <= number; i++){
+            sum += i;
+        }
+        res.send(`${sum}`)
+    }
 })
 
 app.listen(port, () => {
